b33aab4aeaf4c4b64e874d700ce908790b73e559
ABDUL HAMEETHU . M <161139924+ABDULHAMEETHU@users.noreply.github.com>
2026-01-24 13:56:48 +0530

    Time: 1 ms (29.81%), Space: 47.6 MB (26.48%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i =0;
+        while(i < nums.length){
+            int correct_index = nums[i];
+            if( nums[i] < nums.length && nums[i] != nums[correct_index]){
+                swap(nums,i,correct_index);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0;j < nums.length;j++){
+            if(j != nums[j]){
+                 return j;
+            }
+        }
+        return nums.length;
+    }
+    static void swap(int[] arr,int first,int second){
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+}
